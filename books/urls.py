301b4916bae40d36642762bb3a3b18d31ffcbb1c
Viktor Rura <ruravictor@gmail.com>
2024-06-26 18:34:14 +0100

    Implement serializer & views for all endpoints.

---
+++ books/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
