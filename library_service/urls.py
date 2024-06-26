6d7627d5ae880404fb66d57e6f5c9b5aecc2fb2d
Viktor Rura <ruravictor@gmail.com>
2024-06-26 21:39:21 +0100

    Add JWT support.

--- library_service/urls.py
+++ library_service/urls.py
@@ -8,12 +8,13 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("books.urls")),
+    path("api/users/", include("users.urls")),
+    path("api/books/", include("books.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/doc/swagger/",
          SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path(
-        "api/doc/redoc/", 
+        "api/doc/redoc/",
         SpectacularRedocView.as_view(url_name="schema"), name="redoc"
     ),
 ]
