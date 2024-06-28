b464de14c0e3c0fa49312a2aa73a581c17490a74
Viktor Rura <ruravictor@gmail.com>
2024-06-28 18:14:35 +0100

    Implement create a serializer. Validate book inventory is not 0.Decrease inventory by 1 for book.Attach the current user to the borrowing.

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import BorrowingListView, BorrowingDetailView
+from .views import (
+    BorrowingListView,
+    BorrowingDetailView
+
+)
 
 
 urlpatterns = [
-    path('', BorrowingListView.as_view(), name='borrowing-list'),
-    path('<int:id>/',
-         BorrowingDetailView.as_view(), name='borrowing-detail'),
+    path("",
+         BorrowingListView.as_view(), name="borrowing-list"),
+    path("<int:pk>/",
+         BorrowingDetailView.as_view(), name="borrowing-detail"),
 ]
